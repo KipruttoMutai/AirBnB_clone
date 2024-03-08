ef2f21e898dd5089d3bbaf18cff7c654248b45c4
KipruttoMutai <mutaibrian04@gmail.com>
2024-03-08 00:52:39 -0800

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,14 +10,22 @@ class BaseModel:
     """
     Base
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize unique id, created_at, and updated_at attributes
         """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
-
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
 
     def save(self):
         """Update the updated_at attribute with the current datetime"""
